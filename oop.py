52139105239a643478e9d1e45ab1df4372b132cb
Etienne <etienne.boutet@outlook.com>
2020-04-30 20:47:21 -0400

    first fprop complete

--- oop.py
+++ oop.py
@@ -1,6 +1,10 @@
 import numpy as np
-np.random.seed(420)
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from sklearn.metrics import classification_report, accuracy_score
+import numpy as np
 
+np.random.seed(420)
 
 class Layer():
     def __init__(self):
@@ -21,12 +25,12 @@ class Flatten():
         self.data_format = data_format
 
     def forward_propagation(self, inputs):
-        shape = self.input_shape
+        shape = self.data_format
         inputs = inputs.reshape((inputs.shape[0], shape[0] * shape[1])).T
         return inputs
 
 class Dense():
-        def __init__(self, units, activation, weights=None, biases=None):
+        def __init__(self, units, activation):
             self.units = units
             self.activation = activation
             self.weights = weights
@@ -69,12 +73,39 @@ class Model:
 
     def train(self, train_images, train_labels, epochs):
         layer_input = train_images
+    
+        # Every layer handle his own forward and back propagation.
+
+        # F-prop
         for layer in self.layers:
             layer_input = layer.forward_propagation(layer_input)
+        
+        # B-prop
 
-    def validate(self):
-        pass
 
+def load_dataset():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    return x_train, y_train, x_test, y_test
+
+def prep_pixels(train, test):
+    # convert from integers to floats
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    # return normalized images
+    return train_norm, test_norm
 
 model = Model()
 model.add_layer(Flatten((28, 28)))
+
+x_train, y_train, x_test, y_test = load_dataset()
+x_train, x_test = prep_pixels(x_train, x_test)
+
+model.train(x_train, y_train, 1)
+
+#model.add_layer(Dense(64, activation='sigmoid'))
+#model.add_layer(Dense(10, activation='softmax'))
