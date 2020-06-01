80a96fb29202a06c502dfcaa04e997138039b251
Etienne <etienne.boutet@outlook.com>
2020-06-01 09:14:10 -0400

    add linear regression algorithm and visualisation

---
+++ linear_regression/lr.py
@@ -0,0 +1,46 @@
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+class LinearRegression:
+    def __init__(self, learning_rate, n_iters):
+        self.learning_rate = learning_rate
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            y_predicted = np.dot(X, self.weights) + self.bias
+
+            dw = (1/n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1/n_samples) * np.sum(y_predicted - y)
+
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+
+    def predict(self, X):
+        return np.dot(X, self.weights) + self.bias
+
+def mse(y_true, y_predicted):
+    return np.mean((y_true - y_predicted)**2)
+
+X, y = make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+lr = LinearRegression(learning_rate=0.01, n_iters=1000)
+lr.fit(X_train, y_train)
+predictions = lr.predict(X)
+
+cmap = plt.get_cmap('viridis')
+fig = plt.figure(figsize=(8,6))
+m1 = plt.scatter(X_train, y_train, color=cmap(0.9), s=10)
+m2 = plt.scatter(X_test, y_test, color=cmap(0.5), s=10)
+plt.plot(X, predictions, color="black", linewidth=2, label="Prediction")
+plt.show()
